187d200f75e4d8a22366df1063f73bb0d119de44
Lingfeng Yang <lyang@cs.stanford.edu>
2012-03-12 19:48:25 -0700

    Test scripts for citation example.

---
+++ examples/larj/citation/run-citation.sh
@@ -0,0 +1,4 @@
+cd ../../../
+sh tests/citation-header.church $1 $2 $3 > tests/citation-$1-$2-$3.church;
+./jschurch run tests/citation-$1-$2-$3.church > examples/larj/citation/citation-$1-$2-$3.txt
+cd examples/larj/citation
