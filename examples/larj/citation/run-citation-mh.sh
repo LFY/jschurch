1ee1cbeb73524df9fe439c9e227a49366c66db66
Lingfeng Yang <lyang@cs.stanford.edu>
2012-03-13 09:52:26 -0700

    Scripts for evaluationg citation ex. on MH.

---
+++ examples/larj/citation/run-citation-mh.sh
@@ -0,0 +1,4 @@
+cd ../../../
+sh examples/larj/citation/citation-header-mh.church $1 $2 $3 > examples/larj/citation/citation-$1-$2-$3.church;
+./jschurch run examples/larj/citation/citation-$1-$2-$3.church > examples/larj/citation/citation-$1-$2-$3.txt
+cd examples/larj/citation
