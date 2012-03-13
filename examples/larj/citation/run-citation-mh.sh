c30185ba1659c2622f1fcd367995b20d724c80af
lyang <lyang@mcpr.stanford.edu>
2012-03-13 09:55:39 -0700

    Don't let names collide

--- examples/larj/citation/run-citation-mh.sh
+++ examples/larj/citation/run-citation-mh.sh
@@ -1,4 +1,4 @@
 cd ../../../
-sh examples/larj/citation/citation-header-mh.church $1 $2 $3 > examples/larj/citation/citation-$1-$2-$3.church;
-./jschurch run examples/larj/citation/citation-$1-$2-$3.church > examples/larj/citation/citation-$1-$2-$3.txt
+sh examples/larj/citation/citation-header-mh.church $1 $2 $3 > examples/larj/citation/citation-mh-$1-$2-$3.church;
+./jschurch run examples/larj/citation/citation-mh-$1-$2-$3.church > examples/larj/citation/citation-mh-$1-$2-$3.txt
 cd examples/larj/citation
