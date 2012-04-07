6237df514e99844f53e099571c0c3bb8600a3000
yitingy <yitingy@stanford.edu>
2012-04-07 12:54:21 -0700

    handle results saving

--- scheme-compilers/javascript/jschurch-webservice.js
+++ scheme-compilers/javascript/jschurch-webservice.js
@@ -1,10 +1,12 @@
 var __codeCache = {};
 
-
-var safeDelayedEval = function(jsCode, errHandler, delay){
+var safeDelayedEval = function(jsCode, errHandler, resultHandler, delay){
   var safeEval = function() {
     try {
-      eval(jsCode);
+      //var res = eval(jsCode);
+      //console.log("in jschurch-all");
+      //console.log(res);
+      resultHandler(eval(jsCode));
     } catch (err) {
       errHandler(err);
     }};
@@ -23,6 +25,49 @@ var setScheme2jsPorts = function(outHandler){
 };
 
 var scheme2jsPOSTURL = "http://mcpr.stanford.edu/scheme2js/scheme2js.php";
+var evalChurchCodeSaveResult = function(churchCode, errHandler, resultHandler){
+
+  var key = hex_md5(churchCode);
+
+  if (__codeCache[key]) {
+
+    console.log("running from client-side cache");
+    setScheme2jsPorts(errHandler);
+    setScheme2jsPorts(resultHandler);
+    safeDelayedEval(__codeCache[key], errHandler, resultHandler, 100);
+
+  } else {
+
+    // Compile Church to Scheme
+    var churchInputExpr = "";
+    var schemeExpr = "";
+    var compilationSuccess = true;
+    try {
+      churchInputExpr = sc_read(new sc_StringInputPort("(" + churchCode + ")"));
+      schemeExpr = compile(churchInputExpr, null);
+      var wrappedSchemeExpr = scheme2jsTemplate.supplant(
+        { churchprogram : String(schemeExpr).slice(1,-1) });
+    } catch (err) {
+      compilationSuccess = false;
+      errHandler(err);
+    }
+    if (!(compilationSuccess)) {
+      return false;
+    }
+
+    console.log("submitting to scheme2js server");
+    jQuery.post( scheme2jsPOSTURL, { "code" : wrappedSchemeExpr },
+            function(compiledCode) {
+                console.log(compiledCode);
+                __codeCache[key] = compiledCode;
+                setScheme2jsPorts(errHandler);
+                setScheme2jsPorts(resultHandler);
+                safeDelayedEval(compiledCode, errHandler, resultHandler, 0);
+            });
+  }
+};
+ 
+
 
 var evalChurchCode = function(churchCode, returnValueHandler){
 
@@ -32,7 +77,7 @@ var evalChurchCode = function(churchCode, returnValueHandler){
 
     console.log("running from client-side cache");
     setScheme2jsPorts(returnValueHandler);
-    safeDelayedEval(__codeCache[key], returnValueHandler, 100);
+    safeDelayedEval(__codeCache[key], returnValueHandler, function(data){return data;}, 100);
 
   } else {
 
@@ -59,7 +104,7 @@ var evalChurchCode = function(churchCode, returnValueHandler){
                 console.log(compiledCode);
                 __codeCache[key] = compiledCode;
                 setScheme2jsPorts(returnValueHandler);
-                safeDelayedEval(compiledCode, returnValueHandler, 0);
+                safeDelayedEval(compiledCode, returnValueHandler, function(data){return data;}, 0);
             });
 
 
